0d76bc4c98c104cd375d2daa5b14518f99cf8c99
TUSHARJAITWAL <tusharjaitwal015@gmail.com>
2023-03-19 12:23:19 +0530

    Java Script framework

---
+++ index.js
@@ -0,0 +1,395 @@
+// // console.log("Hello world")
+// var n1 = 12;
+// var n2 = 23;
+
+// // console.log(n1 + n2);
+
+// // JavaScript console API
+// // console.warn("This is warning", 12 + 12);
+// // console.error("This is an error");
+// // console.clear("This is clear")
+
+
+// // Javascript Datatypes 
+
+// // Number Datatype is javascript
+// var x= 12;
+// var y= 12;
+// // console.log(x+y);
+
+// // Number DataType
+// var num1 = 123.34;
+// var num2 = 123.34;
+// // console.log(num1+num2);
+
+// // String Datatype in JavaScript
+// var str1 = "Hello i am Tushar";
+// var str2 = "Jaitwal";
+// // console.log(str1+" "+str2);
+
+// // object Datatype in JavaScript
+// var obj = {
+//     rahon:24,
+//     rahul:6,
+//     ravi:12
+// }
+// // console.log(obj);
+
+
+// // Boolean Datatype in JavaScript
+// var a = true;
+// var b = false;
+// // console.log(a);
+// // console.log(b);
+// // console.log(a==b);
+// // console.log(a!=b);
+
+// // var und = undefined
+// var und;
+// // console.log(und);
+// // console.log(und);
+
+// var n = null;
+// // console.log(n);
+
+// // Object Datatype in JavaScript
+// var marks = {
+//     rahul:90,
+//     tushar:80,
+//     rohit:70
+// }
+// // console.log(marks);
+
+// // There are two types of Data Types in javaScript
+// /*
+// 1.Primitive Datatype:-Number,String,Boolean,undefined,null,symbol 
+// 2.Reference Datatype:- arrays and object
+// */
+
+
+// // Array:- collection of customElements
+// var arr = [1,3,4,5,"Tushar",6,7,87,8];
+// // console.log(arr);
+// // console.log(arr[1]);
+// // console.log(arr[4])
+// // console.log(arr.length)
+
+
+// // JavaScript operator
+// // Arithmetic operators
+// var a,b;
+// a = 40
+// b = 100;
+// // console.log("The value of a+b is",a+b);
+// // console.log("The value of a-b is",a-b);
+// // console.log("The value of a*b is",a*b);
+// // console.log("The value of a/b is",a/b);
+// // console.log("The value of a**b is",a**b);
+// // console.log("The a=value of a%b is ",a%b);
+
+
+
+// // Assignment Operators
+// var c;
+// c = b
+// // c+= 50;  c = c+50;
+// // c = c+100;
+// c-=50;
+// // console.log(c);
+
+// // Comparison Operators
+// var j = 50;
+// var k = 100;
+// // console.log(j>k);
+// // console.log(j>=k);
+// // console.log(j<k);
+// // console.log(j<=k);
+// // console.log(j==k);
+// // console.log(j!=k);
+
+
+// // Logical Operators
+
+// // Logical And
+// // console.log(true && true);
+// // console.log(true && false);
+// // console.log(false && true);
+// // console.log(false && false);
+
+// // Logical OR
+// // console.log(true || true);
+// // console.log(true || false);
+// // console.log(false || true);
+// // console.log(false || false);
+
+// // Logical Not
+// // console.log(!false);
+// // console.log(!true);
+
+// // function oin JavaScript
+
+// function  myfunction(a,b) {
+//     c = a**b;
+//     return c 
+// }
+// d1 = myfunction(12,12);
+// d2 = myfunction(15,15);
+// // console.log(d1);
+// // console.log(d2);
+
+// function fun(a,b){
+//     return (a+b)/2;
+// }
+// num1 = fun(12,12);
+// // console.log(num1);
+// n1 = fun(15,15);
+// // console.log(n1);
+
+
+// var p ,q ;
+// p = 12;
+// q = 20;
+// // if (p<q){
+// //     alert("Good AfterNoon");
+// // }
+// // else if(p!=q){
+// //     alert("See you Soon");
+// // }
+// // else{
+// //     alert("Good Evening")
+// // }
+
+// // Voting Program in JavaScript
+// var age;
+// age = 100;
+// if(age>=18 && age<=130){
+//     // console.log("You can vote");
+// }
+    
+// else {
+//     // console.log("You Can't vote");
+// }
+
+// // // Woman married or not cheeking condition
+// // var gender,age;
+// // gender = 'F';
+// // age = 12;
+// // if( gender=="F" && age>=21){
+// //     console.log("She can married");
+// // }
+// // else {
+// //     console.log("She can't married");
+// // }
+
+// // // Cheecking the number is prime or not
+// // var prime = 9;
+// // if (prime%2==0){
+// //     console.log("Number is prime");
+// // }
+// // else{
+// //     console.log("Number is not prime")
+// // }
+
+// // var number1 = 10;
+// // var number2 = 20;
+// // var number3 = 30;
+// // if (number1>number2>number3){
+// //     console.log("number1 is greater");
+// // }
+// // else if (number2>number3){
+// //     console.log("number2 is greater");
+// // }
+// // else {
+// //     console.log("number 3 is greater");
+// // }
+
+// // var age1=18;
+
+// // if (age1<=5){
+// //     console.log("You are a kid")
+// // }
+// // else if(age1>=6 && age1<=17){
+// //     console.log("You are not a kid")
+// // }
+// // else if(age1>=18 && age1<=50){
+// //     console.log("You are young man")
+// // }
+// // else if(age1>=51 && age1<=90){
+// //     console.log("You are old man")
+// // }
+// // else{
+// //     console.log("Invaild")
+// // }
+
+
+
+
+// // Loop in JavaScript 
+
+// let ar = [1,2,3,4,5,6,6,7,8,9,10 ]
+// // console.log(ar)
+// // for(i=0;i<=ar.length;i++){
+// //     console.log(ar[i]);
+// // }
+
+
+// // ar.forEach(function(element){
+// //     console.log(element);
+// // })
+
+// let t =0;
+// // while(t<=ar.length){
+// //     console.log(ar[t]);
+// //     t+=1;
+// // }
+
+// // do{
+// //     console.log(t);
+// //     t+=1;
+
+// // }while(t<=ar.length);
+
+// let set = ["Tushar","Ajay","Rohit","Shubham","Amit"];
+// // console.log(set);
+// // for loop in Javascript
+// // console.log(set.length);
+// // for(i=0;i<=set.length;i++){
+// // console.log(set[i]);
+// // }
+
+// // for each loop in javascript
+// // set.forEach(function(element){
+// //     console.log(element)
+// // })
+
+// // while loop in javascript
+// let f = 0;
+// // while(f<=set.length){
+// //     console.log(set[f]);
+// // f+=1;
+// // }
+
+
+// // do while loop in javascript
+// // do{
+// //     console.log(set[f]);
+// // f+=1;
+
+// // }while(f<=set.length);
+
+// // l = [2,3,4,5,6,8,10,11];
+// // if(l.length%2==0){
+// //     console.log("Number is prime");
+// // }
+// // else{
+// //     console.log("Number is not prime")
+// // }
+
+
+
+// // for(m=1;m<=100;m++){
+// //     if(m%2==0){
+// //         console.log(m)
+// //     }
+// // }
+
+// // let m =1;
+// // while (m<=100){
+// //     if (m%2==1){
+// //         console.log(m)
+// //     }
+// // m+=1;
+// // } 
+
+// // let table = 5;
+// // while(table<=50){
+// //     console.log(table);
+// // table = table+5;
+// // }
+
+
+// // factorial Program in javascript
+// // fact = 1;
+// // v = 1
+// // while(v<=5){
+// //     fact = fact*v;
+// // v+=1;
+// // console.log(fact);
+
+// // }
+
+
+// // Reverse print in table 
+
+// // et o = 30;
+// // while (o>=3){
+// //     console.log(o);
+// //     o = o-3;
+// // }
+
+
+// let arry = [12,45,5,56,null,"String","Rohan"]
+// // console.log(arry)
+// // console.log(arry.length)
+// // arry.pop()
+// // console.log(arry)
+// // arry.push("Tushar")
+// // console.log(arry)
+// // console.shift()
+// // arry.reverse();
+// // // console.log(arry)
+// // xc = arry.slice(1,4);
+// // console.log(xc)
+
+// // e =arry.unshift("Ajay","Arun");
+// // console.log(e)
+// // console.log(arry)
+ 
+// // let af = arry.indexOf("Rohan")
+// // console.log(af)
+
+// // let arrya = [1,23,34,65,2345]
+// // arrya.sort()
+// // console.log(arrya)
+
+
+// // String method in javaScript
+let str3 = "Hello I am  Tushar Jaitwal";
+// // console.log(str3)
+// // console.log(str3.length)
+// // p= str3.toUpperCase()
+// // console.log(p)
+// // p= str3.toLowerCase()
+// // console.log(p)
+
+// console.log(str3.indexOf('am'))
+// console.log(str3.lastIndexOf('Jaitwal'))
+
+
+// // p = str3.replace("Tushar","Rohit")
+// // console.log(p)
+
+// // p = str3.slice(0,20);
+// // console.log(p)
+
+
+// JavaSxcript Date and Time
+
+// Method in Date 
+// let date = new Date()
+// console.log(date)
+// console.log(date.getFullYear())
+// console.log(date.getMonth())
+// console.log(date.getDate())
+// console.log(date.getHours())
+// console.log(date.getDay())
+// console.log(date.getTime())
+// console.log(date.getMinutes())
+// console.log(date.getSeconds())
+
+// function displaydate(){
+//     document.getElementById('demo').innerHTML=Date();
+// }
+
+
+
